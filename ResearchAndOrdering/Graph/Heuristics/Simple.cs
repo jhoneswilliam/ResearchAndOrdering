d6c23efc15cc8277639fa8006ab17070980d6ab4
Jhones William <jhones.william@outlook.com.br>
2018-03-27 17:52:08 -0300

    Alteração do modelo

---
+++ ResearchAndOrdering/Graph/Heuristics/Simple.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchAndOrdering.Graph
+{
+    class Simple<Estructure> where Estructure : MatrixIncidence
+    {
+        public Graph<Estructure> Graph { get; set; }
+        //public Queue<LinkedList<int>> QueueRouter { get; set; }
+
+        public Simple(Graph<Estructure> graph)
+        {
+            Graph = graph;
+            //QueueRouter = new Queue<LinkedList<int>>();
+        }
+
+        public LinkedList<int> run(int verticeStart, int verticeEnd)
+        {
+            LinkedList<int> router = new LinkedList<int>();
+            router.AddLast(verticeStart);
+
+            Queue<LinkedList<int>> queueRouter = new Queue<LinkedList<int>>();
+            queueRouter.Enqueue(router);
+
+            return run(queueRouter, verticeEnd);
+        }
+
+        public LinkedList<int> run(Queue<LinkedList<int>> queueRouter, int verticeEnd)
+        {
+            LinkedList<int> possibleRouter;
+
+            while (queueRouter.Count > 0)
+            {
+                possibleRouter = queueRouter.Dequeue();
+                int lastVertice = possibleRouter.Last();
+
+                for (int edge = 0; edge < Graph.EdgesCount(); edge++)
+                {
+                    if (Graph.GraphEstructure.Matrix[lastVertice, edge] > 0)
+                    {
+                        for (int vertice = 0; vertice < Graph.VerticeCount(); vertice++)
+                        {
+                            if (Graph.GraphEstructure.Matrix[vertice, edge] > 0 && possibleRouter.Contains(vertice) == false)
+                            {
+                                possibleRouter.AddLast(vertice);
+
+                                if (vertice == lastVertice)
+                                {
+                                    return possibleRouter;
+                                }
+                                else
+                                {
+                                    queueRouter.Enqueue(possibleRouter);
+                                }
+                                vertice = Graph.VerticeCount();
+                                edge = Graph.EdgesCount();
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        //public LinkedList<int> run(int verticeStart, int verticeEnd, string tehs)
+        //{
+        //    for (int edge = 0; edge < Graph.EdgesCount(); edge++)
+        //    {
+        //        if(Graph.GraphEstructure.Matrix[verticeStart, edge] > 0)
+        //        {
+        //            for (int vertice = 0; vertice < Graph.VerticeCount(); vertice++)
+        //            {
+        //                if(Graph.GraphEstructure.Matrix[vertice, edge] > 0)
+        //                {
+        //                    LinkedList<int> possibleRouter = new LinkedList<int>(router);
+        //                    possibleRouter.AddLast(vertice);
+        //                    QueueRouter.Enqueue(possibleRouter);
+
+        //                    vertice = Graph.VerticeCount();
+        //                    edge = Graph.EdgesCount();
+        //                }
+        //            }
+        //        }
+        //    }
+
+
+
+        //    return run(router, verticeEnd);
+        //}
+
+
+    }
+}
--- ResearchAndOrdering/Teste/GraphTeste.cs
+++ ResearchAndOrdering/Teste/GraphTeste.cs
@@ -1,4 +1,7 @@
 ﻿using ResearchAndOrdering.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ResearchAndOrdering.Teste
 {
@@ -6,13 +9,34 @@ namespace ResearchAndOrdering.Teste
     {
         public static string Simple()
         {
+
+            int verticeStart = 0;
+            int verticeEnd = 3;
+
             MatrixIncidence matrixIncidence = new MatrixIncidence(5, 3);
             matrixIncidence.AddEdge(0, 1);
             matrixIncidence.AddEdge(1, 2);
             matrixIncidence.AddEdge(2, 3);
             matrixIncidence.ToConsole();
-            //Graph<MatrixIncidence> graph = new Graph<MatrixIncidence>(matrixIncidence);
-           //return new Simple<MatrixIncidence>(graph).run();
+            Graph<MatrixIncidence> graph = new Graph<MatrixIncidence>(matrixIncidence);
+            LinkedList<int> router = new Simple<MatrixIncidence>(graph).run(verticeStart, verticeEnd);
+            Console.WriteLine("==============================================");
+            if(router != null)
+            {
+                Console.WriteLine("::EURECA:::");
+                while(router.Count > 0)
+                {
+                    Console.WriteLine(string.Format("vertice[{0}]", router.First()));
+                    router.RemoveFirst();
+                }
+            }
+            else
+            {
+                Console.WriteLine("DON'T CRY");
+            }
+
+            Console.ReadKey();
+
             return "";
         }
 
